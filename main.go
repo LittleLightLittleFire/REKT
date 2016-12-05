b5cf529133760e5aef4d0b122ca11a22ddb9150f
lllf <littlelightlittlefire@gmail.com>
2016-12-05 21:39:41 +1100

    Fixes bug where resubmitted liquidations are counted double

--- main.go
+++ main.go
@@ -92,6 +92,16 @@ func runClient(cfg BotConfig, twitter *twitter.Client, state *State) error {
 	conn.SetReadDeadline(time.Now().Add(pongWait))
 	conn.SetPongHandler(func(string) error { conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
 
+	// The BitMex API may "insert" / "delete / "insert" the order when is able to liquidate at a better price
+	// "insert" is sent when the order is submitted
+	// "delete" is sent when the order is executed
+	// It may also "update" the order when the it is amended or partially filled
+	// The following sequence is possible: insert ..... update ..... delete/insert ..... update ..... delete/insert ..... delete
+	// ..... indicated a posssible time delay
+
+	// Thus we need to keep track of when the order was last deleted and purge it as neccessary
+	lastDelete := make(map[string]time.Time)
+
 	for {
 		var data map[string]interface{}
 		if err := conn.ReadJSON(&data); err != nil {
@@ -107,12 +117,23 @@ func runClient(cfg BotConfig, twitter *twitter.Client, state *State) error {
 		if table, ok := data["table"]; ok {
 			switch table {
 			case "liquidation":
+				// This will panic if the cast fails, but it is fine, because it meant bitmex sent us bad data
+				innerDataList := data["data"].([]interface{})
+
 				switch data["action"] {
 				case "partial":
-				case "insert":
-					// This will panic if the cast fails, but it is fine, because it meant bitmex sent us bad data
-					innerDataList := data["data"].([]interface{})
+				case "delete":
+					for _, innerData := range innerDataList {
+						innerData := innerData.(map[string]interface{})
+						orderID := innerData["orderID"].(string)
 
+						lastDelete[orderID] = time.Now()
+					}
+
+				case "update":
+					// The liquidation may amended by bitmex (position may be reduced or price changed)
+
+				case "insert":
 					for _, innerData := range innerDataList {
 						innerData := innerData.(map[string]interface{})
 
@@ -120,6 +141,12 @@ func runClient(cfg BotConfig, twitter *twitter.Client, state *State) error {
 						leavesQty := int64(innerData["leavesQty"].(float64)) // Cast to int64 because this is always int
 						symbol := innerData["symbol"].(string)
 						side := innerData["side"].(string)
+						orderID := innerData["orderID"].(string)
+
+						// Check if this is an insert after a delete
+						if _, ok := lastDelete[orderID]; ok {
+							continue
+						}
 
 						l := Liquidation{
 							Price:    price,
@@ -143,6 +170,14 @@ func runClient(cfg BotConfig, twitter *twitter.Client, state *State) error {
 						}
 					}
 				}
+			}
+		}
+
+		// Purge expired orders so we don't hemorrhage memory
+		now := time.Now()
+		for orderID, timestamp := range lastDelete {
+			if now.Sub(timestamp) > 10*time.Second {
+				delete(lastDelete, orderID)
 			}
 		}
 	}
